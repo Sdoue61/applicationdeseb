76035747f3c3840b2d21e12adfc5c2aa861c9ed2
Sdoue61 <sebastiendoue@gmail.com>
2021-08-12 13:49:41 +0400

    entrer nom ok

--- app/src/main/java/com/example/applicationdeseb/start.kt
+++ app/src/main/java/com/example/applicationdeseb/start.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class start : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +13,16 @@ class start : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         val next = findViewById<Button>(R.id.btnext)
-        next.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        val nameuser = findViewById<EditText>(R.id.nom)
 
+            next.setOnClickListener {
+                if (nameuser.text.isNotEmpty()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(this, "Entrer votre nom", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
